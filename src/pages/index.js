dcbf61aa4f8bb24b030c024d1aea5e1334f093cb
akash-endlos <akash.endlos@gmail.com>
2023-07-03 21:33:02 +0530

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,78 +1,113 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '@/redux/reducers/authSlice';
-import { store } from '@/redux/store';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from '@mui/material';
 
 export default function Home() {
-  const token = store?.getState()?.auth?.user?.token?.accessToken;
-  const dispatch = useDispatch()
-  const router = useRouter()
-  const { register, handleSubmit, formState: { errors },reset } = useForm();
+  const token = useSelector(state => state?.auth?.user?.token?.accessToken);
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
   useEffect(() => {
     if (token) {
       router.push('/dashboard');
     }
-  }, []);
+  }, [token, router]);
+
   const onSubmit = async (data) => {
     try {
       await dispatch(loginUser(data)).unwrap();
-      router.push('/dashboard')
-      reset()
-      toast.success('LoggedIn Successfully')
+      router.push('/dashboard');
+      reset();
+      toast.success('Logged in successfully');
     } catch (error) {
-      toast.error(error.message)
+      toast.error(error.message);
     }
   };
 
-
   return (
-    <>
-      <section className="bg-gray-50 ">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <img className="w-28 h-28 mr-2" src="/1080.png" alt="logo" />
-          </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0  ">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+    <section className="bg-gray-50">
+      <Container
+        maxWidth="sm"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          py: 8,
+        }}
+      >
+        {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-28 h-28 mr-2" src="/1080.png" alt="logo" />
+        </a> */}
+        <Box
+          sx={{
+            width: '100%',
+            backgroundColor: 'white',
+            borderRadius: 'lg',
+            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+            my: 8,
+            p: 4,
+          }}
+        >
+          <Box sx={{ mb: 6 }}>
+            <Typography
+              variant="h5"
+              component="h1"
+              sx={{ fontWeight: 'bold', mb: 2 }}
+            >
+              Sign in
+            </Typography>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <TextField
+                id="email"
+                label="Email"
+                variant="outlined"
+                {...register('email', { required: 'Email is required' })}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+                fullWidth
+                sx={{ mb: 3 }}
+              />
+              <TextField
+                id="password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                {...register('password', { required: 'Password is required' })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+                fullWidth
+                sx={{ mb: 3 }}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="large"
+                fullWidth
+              >
                 Sign in
-              </h1>
-              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    {...register('email', { required: 'Email is required' })}
-                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  ${errors.email ? 'border-red-500' : ''}`}
-                    placeholder="name@company.com"
-                    required
-                  />
-                  {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
-                </div>
-                <div>
-                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    {...register('password', { required: 'Password is required' })}
-                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  ${errors.password ? 'border-red-500' : ''}`}
-                    placeholder="••••••••"
-                    required
-                  />
-                  {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
-                </div>
-                <button type="submit" className="w-full bg-blue-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign in</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Container>
+    </section>
+  );
 }
