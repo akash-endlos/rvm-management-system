20bb7890e2a11de8a75b389f31a4af4d42cf5622
akash-endlos <akash.endlos@gmail.com>
2023-07-03 16:01:33 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,7 @@ export default function Home() {
                     name="email"
                     id="email"
                     {...register('email', { required: 'Email is required' })}
-                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.email ? 'border-red-500' : ''}`}
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  ${errors.email ? 'border-red-500' : ''}`}
                     placeholder="name@company.com"
                     required
                   />
@@ -61,7 +61,7 @@ export default function Home() {
                     name="password"
                     id="password"
                     {...register('password', { required: 'Password is required' })}
-                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.password ? 'border-red-500' : ''}`}
+                    className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  ${errors.password ? 'border-red-500' : ''}`}
                     placeholder="••••••••"
                     required
                   />
