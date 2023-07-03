986f50f2607faf944acf1e3dc5699f4c323f178b
akash-endlos <akash.endlos@gmail.com>
2023-07-03 21:30:56 +0530

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -67,7 +67,7 @@ export default function Home() {
                   />
                   {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
                 </div>
-                <button type="submit" className="w-full bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                <button type="submit" className="w-full bg-blue-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign in</button>
               </form>
             </div>
           </div>
