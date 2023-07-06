36181c78b877b56548cb1d06114ad93f95895305
akash-endlos <akash.endlos@gmail.com>
2023-07-06 11:36:13 +0530

    first commit

--- src/components/SidebarLayout/sidebar/NewSidebar.jsx
+++ src/components/SidebarLayout/sidebar/NewSidebar.jsx
@@ -75,3 +75,93 @@ const NewSidebar = ({ isSidebarOpen, menuItems, setIsSidebarOpen }) => {
 };
 
 export default NewSidebar;
+
+
+
+
+
+// import Link from 'next/link';
+// import React, { useState, useEffect } from 'react';
+// import { FiChevronDown } from 'react-icons/fi';
+
+// const NewSidebar = ({ isSidebarOpen, menuItems, setIsSidebarOpen }) => {
+//   const [openItems, setOpenItems] = useState([]);
+
+//   useEffect(() => {
+//     // Set initial open items based on items that have children
+//     const initialOpenItems = menuItems.reduce((acc, item, index) => {
+//       if (item.children && item.children.length > 0) {
+//         return [...acc, `${index}`];
+//       }
+//       return acc;
+//     }, []);
+
+//     setOpenItems(initialOpenItems);
+//   }, [menuItems]);
+
+//   const toggleItem = (index) => {
+//     if (openItems.includes(index)) {
+//       setOpenItems(openItems.filter((item) => item !== index));
+//     } else {
+//       setOpenItems([...openItems, index]);
+//     }
+//   };
+
+//   const renderMenuItems = (items, parentIndex = '') => {
+//     return (
+//       <ul>
+//         {items.map((item, index) => {
+//           const itemIndex = parentIndex ? `${parentIndex}-${index}` : `${index}`;
+//           const isOpen = openItems.includes(itemIndex);
+
+//           return (
+//             <li key={index}>
+//               <div
+//                 className="menu-item rounded-lg flex items-center justify-between py-2 px-4 text-gray-400 hover:bg-gray-700 hover:text-white cursor-pointer"
+//                 onClick={() => toggleItem(itemIndex)}
+//               >
+//                 {item.url ? (
+//                   <Link href={item.url}>
+//                     <span className="flex justify-center items-center gap-2">
+//                       {item.icon}
+//                       {item.title}
+//                     </span>
+//                   </Link>
+//                 ) : (
+//                   <span className="flex justify-center items-center gap-2">
+//                     {item.icon}
+//                     {item.title}
+//                   </span>
+//                 )}
+//                 {item.children && item.children.length > 0 && (
+//                   <FiChevronDown
+//                     className={`ml-2 transition-transform duration-300 transform ${
+//                       isOpen ? 'rotate-180' : 'rotate-0'
+//                     }`}
+//                   />
+//                 )}
+//               </div>
+//               {item.children && item.children.length > 0 && isOpen && renderMenuItems(item.children, itemIndex)}
+//             </li>
+//           );
+//         })}
+//       </ul>
+//     );
+//   };
+
+//   return (
+//     <>
+//       <aside
+//         className={`overflow-y-auto fixed top-0 left-0 w-64 h-full z-40 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700 transform transition duration-300 ease-in-out ${
+//           isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+//         }`}
+//       >
+//         <div className="px-4 pt-24 pb-5">
+//           {renderMenuItems(menuItems)}
+//         </div>
+//       </aside>
+//     </>
+//   );
+// };
+
+// export default NewSidebar;
