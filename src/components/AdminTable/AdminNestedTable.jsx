fffe7da7edbbf21235661baf967c6cc5507d26aa
akash-endlos <akash.endlos@gmail.com>
2023-07-07 18:22:34 +0530

    updated

--- src/components/AdminTable/AdminNestedTable.jsx
+++ src/components/AdminTable/AdminNestedTable.jsx
@@ -1,20 +1,36 @@
 import React, { useMemo } from 'react';
 import { MaterialReactTable } from 'material-react-table';
 
-const AdminNestedTable = ({ columns, data,heading,handleAdminNestedTableRowActions,handleSubNestedTable,currentDepth }) => {
+const AdminNestedTable = ({ columns, data, heading, handleAdminNestedTableRowActions, handleSubNestedTable, currentDepth }) => {
+
+  const renderDetailPanel = useMemo(() => {
+    if (currentDepth === 1) {
+      return undefined; // Do not render detail panel for the first level
+    } else if (handleSubNestedTable) {
+      return ({ row, table }) => handleSubNestedTable(row, table, currentDepth); // Pass currentDepth to nested handleSubNestedTable
+    } else {
+      return undefined;
+    }
+  }, [currentDepth, handleSubNestedTable]);
 
   return (
-  <> 
-    {data && <MaterialReactTable  enableRowActions
-        enableFilters
-        enableColumnFilterModes
-        enableColumnOrdering
-        enablePinning
-        positionActionsColumn="last"
-        renderDetailPanel={currentDepth===1?undefined:({ row, table }) => handleSubNestedTable(row, table)}
-        renderRowActions={({ row, table }) => handleAdminNestedTableRowActions(row, table)}
-        columns={columns} data={data} />}
-  </>);
+    <> 
+      {data && (
+        <MaterialReactTable
+          enableRowActions
+          enableFilters
+          enableColumnFilterModes
+          enableColumnOrdering
+          enablePinning
+          positionActionsColumn="last"
+          renderDetailPanel={renderDetailPanel}
+          renderRowActions={({ row, table }) => handleAdminNestedTableRowActions(row, table)}
+          columns={columns}
+          data={data}
+        />
+      )}
+    </>
+  );
 };
 
 export default AdminNestedTable;
