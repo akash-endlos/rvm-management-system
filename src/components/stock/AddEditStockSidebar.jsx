05a1f816c0fe7493ca53ee3eb60a438a3e701d97
akash-endlos <akash.endlos@gmail.com>
2023-07-11 16:10:35 +0530

    updated

---
+++ src/components/stock/AddEditStockSidebar.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import {
+  Button,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { fetchAllVendors } from '@/redux/reducers/vendorSlice';
+
+const schema = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  contact: yup.string().required('Contact is required'),
+  email: yup.string().email('Invalid email').required('Email is required'),
+});
+
+const AddEditStockSidebar = ({ onClose, onSubmit, selectedVendor }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllVendors());
+  }, []);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: selectedVendor?.name || '',
+      contact: selectedVendor?.contact || '',
+      email: selectedVendor?.email || '',
+    },
+  });
+
+  const handleFormSubmit = (data) => {
+    onSubmit(data);
+  };
+
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        right: 0,
+        width: '500px',
+        height: '100vh',
+        backgroundColor: '#f0f0f0',
+        padding: '1rem',
+        display: 'flex',
+        flexDirection: 'column',
+        zIndex: 999,
+      }}
+    >
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <Typography variant="h5" style={{ fontWeight: 'bold', color: 'teal' }}>
+          Customer
+        </Typography>
+        <div style={{ marginBottom: '10px' }}>
+          <TextField fullWidth label="Name" name="name" {...register('name')} />
+          <div style={{ color: 'red' }}>{errors.name?.message}</div>
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <TextField fullWidth label="Contact" name="contact" {...register('contact')} />
+          <div style={{ color: 'red' }}>{errors.contact?.message}</div>
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <TextField fullWidth label="Email" name="email" {...register('email')} />
+          <div style={{ color: 'red' }}>{errors.email?.message}</div>
+        </div>
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={{ marginRight: '10px' }}
+        >
+          {selectedVendor ? 'Update' : 'Submit'}
+        </Button>
+        <Button onClick={onClose}>Cancel</Button>
+      </form>
+    </div>
+  );
+};
+
+export default AddEditStockSidebar;
--- src/pages/stock/index.jsx
+++ src/pages/stock/index.jsx
@@ -1,9 +1,168 @@
+import AdminTable from '@/components/AdminTable/AdminTable'
 import Layout from '@/layout/Layout'
-import React from 'react'
+import { fetchAllLocalVendors } from '@/redux/reducers/localVendorSlice'
+import React, { useEffect, useMemo, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Box, Button, IconButton, Tooltip, Typography } from '@mui/material'
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { Delete, Edit } from '@mui/icons-material'
+import AddEditStockSidebar from '@/components/stock/AddEditStockSidebar'
+import DeleteStockModal from '@/components/stock/DeleteStockModal'
 
 const index = () => {
+  const [isAddSidebarOpen, setIsAddSidebarOpen] = useState(false);
+  const [selectedStock, setSelectedStock] = useState(null);
+  const [isDeleteStockModalOpen, setIsDeleteStockModalOpen] = useState(false);
+  const alllocalVendors = useSelector((state) => state.localvendor)
+  console.log(alllocalVendors);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchData = () => {
+      dispatch(fetchAllLocalVendors()).unwrap()
+        .then(() => {
+          // Success
+        })
+        .catch(error => {
+          toast.error(error);
+        });
+    };
+  
+    fetchData();
+  }, [dispatch]);
+  
+  const mainTableColumns = useMemo(
+      () => [
+        {
+          accessorKey: 'index',
+          header: 'Sr. No',
+          size: 150,
+        },
+        {
+          accessorKey: 'name',
+          header: 'Name',
+          size: 150,
+        },
+        {
+          accessorKey: 'email',
+          header: 'Email',
+          size: 150,
+        },
+        {
+          accessorKey: 'contact',
+          header: 'Mobile No.',
+          size: 150,
+        },
+      ],
+      []
+    );
+    const handleEditVendor = (stockData) => {
+      setSelectedStock(stockData);
+      setIsAddSidebarOpen(true);
+    };
+    const handleDeleteVendor = (customerData) => {
+      setSelectedStock(customerData);
+      setIsDeleteStockModalOpen(true);
+    };
+    const handleAdminTableRowActions = (row, table) => {
+      return (
+        <Box sx={{ display: 'flex', gap: '1rem' }}>
+          <Tooltip arrow placement="left" title="Edit Customer">
+            <IconButton onClick={() => handleEditVendor(row.original)}>
+              <Edit />
+            </IconButton>
+          </Tooltip>
+          <Tooltip arrow placement="right" title="Delete Customer">
+            <IconButton color="error" onClick={() => handleDeleteVendor(row.original)}>
+              <Delete />
+            </IconButton>
+          </Tooltip>
+          {/* <Tooltip arrow placement="right" title="Add Branch">
+            <IconButton color="secondary" onClick={() => handleOpenAddBranchSidebar(row.original)}>
+              <FiGitBranch />
+            </IconButton>
+          </Tooltip> */}
+        </Box>
+      );
+    };
+    const handleOpenAddSidebar = () => {
+      setSelectedStock(null);
+      setIsAddSidebarOpen(true);
+    };
+  const handleToolBar = (table) => {
+    const handleExportRows = (rows) => {
+      csvExporter.generateCsv(rows.map((row) => row.original));
+    };
+    return (
+      <Box sx={{ display: 'flex', gap: '1rem', p: '0.5rem', flexWrap: 'wrap' }}>
+        <Button
+          disabled={table.getPrePaginationRowModel().rows.length === 0}
+          onClick={() => handleExportRows(table.getPrePaginationRowModel().rows)}
+          startIcon={<FileDownloadIcon />}
+          variant="contained"
+        >
+          Export All Rows
+        </Button>
+        <Button
+          disabled={!table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()}
+          onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
+          startIcon={<FileDownloadIcon />}
+          variant="contained"
+        >
+          Export Selected Rows
+        </Button>
+        <Button variant="contained" onClick={handleOpenAddSidebar}>
+          Add Vendor
+        </Button>
+      </Box>
+    );
+  };
+  const handleCloseAddSidebar = () => {
+    setIsAddSidebarOpen(false);
+  };
+  const handleAddStock = async (stockData) => {
+    if (selectedStock) {
+      // Update existing customer
+
+      console.log('Updating customer', stockData);
+    } else {
+       
+        console.log('Add customer', stockData);
+    }
+    handleCloseAddSidebar();
+  };
+  const handleCloseDeleteStockModal=()=>{
+    setIsDeleteStockModalOpen(false)
+  }
+  const handleConfirmDeleteVendor = () => {
+console.log(selectedStock);
+  };
   return (
-    <Layout>index</Layout>
+    <Layout>
+    <Typography variant="h4" style={{ fontWeight: 'bold', color: 'teal' }}>
+   Vendor
+ </Typography>
+ <>
+   <AdminTable
+     data={alllocalVendors}
+     handleToolBar={handleToolBar}
+     columns={mainTableColumns}
+     handleAdminTableRowActions={handleAdminTableRowActions}
+   />
+    {isAddSidebarOpen && (
+          <AddEditStockSidebar
+            onClose={handleCloseAddSidebar}
+            onSubmit={handleAddStock}
+            selectedStock={selectedStock}
+          />
+        )}
+         <DeleteStockModal
+          isOpen={isDeleteStockModalOpen}
+          onClose={handleCloseDeleteStockModal}
+          onDelete={handleConfirmDeleteVendor}
+          title='Stock'
+        />
+   </>
+</Layout>
   )
 }
 
